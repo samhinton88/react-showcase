77576941dc5a37db1651fd33418be59f75d74c20
sam hinton <sam.hinton88@gmail.com>
2018-02-06 15:40:09 +0000

    added boilerplate for LandingArea component

---
+++ src/containers/LandingArea/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class LandingArea extends Component {
+
+  render() {
+    return (
+      <div>LandingArea</div>
+    )
+  }
+}
+
+export default LandingArea;
