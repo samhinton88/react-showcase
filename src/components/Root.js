43b20b0461bc3ddc68cb9f0bbf5e98dd143f7193
sam hinton <sam.hinton88@gmail.com>
2018-02-06 12:39:42 +0000

    wired up Root component as a provider to accept application store, roots provided in routes.js

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+
 import { Router, browserHistory } from 'react-router';
+import routes from '../routes';
+
 
 const Root = ({ store }) => {
   return (
     <Provider store={store}>
-      <Router history={browserHistory} />
+      <Router history={browserHistory} routes={routes} />
     </Provider>
   );
 }
+
+export default Root;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import reducers from './reducers';
 import { createStore, applyMiddleware } from 'redux';
 
+import Root from './components/Root';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Root store={createStoreWithMiddleware(reducers)} />,
+  document.getElementById('container')
+);
