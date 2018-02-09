c4834330d6793f720cb037ea17ffaf7d4fd9d483
sam hinton <sam.hinton88@gmail.com>
2018-02-09 16:47:17 +0000

    implemented typewriter component chain

--- src/components/StoryTeller/index.js
+++ src/components/StoryTeller/index.js
@@ -1,20 +1,101 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-class StoryTeller extends Component {
 
-  renderStory() {
-    const story = this.props.narrative.userNarrative.map((narrObj) => {
-      return <div key={narrObj.id}>{narrObj.skillName}</div>
-    })
+class TypewriterChar extends Component {
 
-    return story;
+  render() {
+    console.log('inside TypewriterChar')
+    console.log(this.props.style)
+    return (
+      <p style={this.props.style}>
+        {this.props.content}
+      </p>
+    )
+  }
+}
+
+class Word extends Component {
+
+
+
+  renderChars = () => {
+    const chars =  this.props.content.split('');
+    const mappedChars = chars.map((char, i) => {
+      let style;
+
+      if (this.props.new) {
+        style =  {
+            animationName: 'fadeIn',
+            animationDuration: '1s',
+            animationDelay: `${i / 10}s`,
+            color: 'black',
+            animationFillMode: 'both'
+        }
+
+      }
+
+
+
+
+      return <TypewriterChar content={char} style={style} key={i}/>
+    });
+
+    return mappedChars;
   }
 
   render() {
-    console.log(this.props.narrative)
+    console.log('inside word render')
+    return(
+      <div className='word' style={{display: 'flex', flexDirection: 'row'}}>
+        {this.renderChars()}
+      </div>
+    )
+  }
+
+}
+
+class StoryTeller extends Component {
+
+  renderStory() {
+    const skillSeq = this.props.narrative.userNarrative.map((narrObj) => {
+      return (
+        {content: narrObj.skillName}
+      )
+    })
+
+    let style;
+
+    const story = skillSeq.map((skill, i) => {
+      if(i === skillSeq.length - 1) {
+        style = {
+          animationName: 'fadeIn',
+          animationDelay: `${i}s`,
+          animationDuration: '1s',
+          animationFillMode: 'both',
+          marginRight: '5px'
+        }
+      }
+
+      const content = skill
+
+      return (
+        <Word
+          content={skill.content}
+          key={i}
+          style={style}
+          postion={skill.postion}
+          new={i === skillSeq.length - 1 ? true : false}
+        />
+      )
+    })
+
+    return story
+  }
+
+  render() {
     return (
-      <div className='story-teller'>
+      <div className='story-teller' style={{display: 'flex', flexDirection: 'row'}}>
         {this.renderStory()}
       </div>
     )
--- src/reducers/user_narrative_reducer.js
+++ src/reducers/user_narrative_reducer.js
@@ -1,7 +1,14 @@
 import { UPDATE_USER_NARRATIVE } from '../actions/types';
 
+const initState = [
+  {id: 1, skillName: 'Rails', color: '#a2282d', tagLine: 'Rapid prototyping in eloquent Ruby'},
+  {id: 2, skillName: 'MongoDB', color: '#479949', tagLine: 'NoSQL magic'},
 
-export default function(state = { userNarrative: [] }, action) {
+  {id: 3, skillName: 'Express', color: '#f6dd3b', tagLine: 'RESTful servers and summer strolls'}
+]
+
+
+export default function(state = { userNarrative: initState }, action) {
   switch (action.type) {
     case UPDATE_USER_NARRATIVE:
       return {
