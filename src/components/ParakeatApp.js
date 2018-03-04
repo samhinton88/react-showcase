bc9ad6e412e431f7ef2b30621e6457b57929683f
sam hinton <sam.hinton88@gmail.com>
2018-03-04 15:05:10 +0000

    added to parakeat landing

--- src/components/ParakeatApp.js
+++ src/components/ParakeatApp.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 import ParakeatLogo from './ParakeatLogo';
+import Iphone from './Iphone';
+import ParakeatMarker from './ParakeatMarker';
 
 class ParakeatApp extends Component {
   state = {
@@ -12,7 +14,7 @@ class ParakeatApp extends Component {
   }
 
   render() {
-    console.log(this.parakeatPupil)
+
 
     return(
       <div style={{ textAlign: 'center'}} onMouseMove={this.handleMouseMove}>
@@ -23,11 +25,12 @@ class ParakeatApp extends Component {
               backgroundSize: 'stretch',
               backgroundRepeat: 'no-repeat',
               backgroundPosition: 'top center',
-              backgroundImage: 'linear-gradient(0deg, rgba(35,172,166,.85),rgba(255,255,255,0)), url(https://www.instock.nl/app/uploads/2016/07/RubendeRuijter_InstockDH-51_klein.jpg)',
+              backgroundImage: 'linear-gradient(-90deg, rgba(35,172,166,.85),rgba(255,255,255,0)), url(https://www.instock.nl/app/uploads/2016/07/RubendeRuijter_InstockDH-51_klein.jpg)',
               padding: '0',
               display: 'flex',
-              flexDirection: 'column',
+              flexDirection: 'row',
               alignItems: 'center',
+              justifyContent: 'flex-start',
               opacity: '1',
               paddingBottom: '30px',
               paddingTop: '30px'
@@ -37,35 +40,81 @@ class ParakeatApp extends Component {
 
         >
         <div
-          className='circle-logo-wrapper'
-          style={
-            {
-              height: '200px',
-              width: '200px',
-              borderRadius: '50%',
-              backgroundColor: 'rgba(237, 237, 237, 1)',
-              display: 'flex',
-              alignItems: 'center',
-              padding: '0px',
-              marginBottom: '15px',
-              marginTop: '10px'
-            }
-
-          }
+          style={{display: 'flex', alignItems: 'center', justifyContent: 'flex-start', flexDirection: 'column'}}
         >
-          <ParakeatLogo watching mousePos={this.state.mousePos} elRef={(e) => this.parakeatPupil = e}/>
-
-        </div>
-          <h1>Parakeat</h1>
-
-          <h2
-
+          <div
+            className='circle-logo-wrapper'
             style={
               {
-                textShadow: '2px 2px 1px rgba(135, 135, 135, .3)',
+                height: '200px',
+                width: '200px',
+                borderRadius: '50%',
+                backgroundColor: 'rgba(237, 237, 237, 1)',
+                display: 'flex',
+                alignItems: 'center',
+                padding: '0px',
+                marginBottom: '0px',
+                marginTop: '10px',
+                marginRight: '20px',
+                marginLeft: '30px',
+                boxShadow: '1px 2px 10px rgba(0, 0, 0, .7)'
+              }
+
+            }
+          >
+            <ParakeatLogo watching mousePos={this.state.mousePos} elRef={(e) => this.parakeatPupil = e}/>
+          </div>
+            <h1
+              style={
+                {
+                  textShadow: '2px 2px 1px rgba(0, 0, 0, .9)'
+                }
+              }
+            >
+              Parakeat
+            </h1>
+          </div>
+
+
+
+        </div>
+
+
+        <div
+          className='divider full-width'
+          style={
+            {
+              background: 'linear-gradient(90deg, rgba(228, 222, 214, 0), rgba(128, 189, 158, .9), rgba(245, 240, 95, .9))',
+              marginTop: '20px',
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'flex-start',
+              height: '475px'
+            }
+          }
+        >
+        <div
+          style={
+              {
+                boxShadow: '1px 1px 2px black',
+                marginLeft: '20px',
+                backgroundImage: 'linear-gradient(-10deg, rgba(153, 171, 131, 0.1), url(../../assets/map_newcastle.png))',
+                width: '250px',
+                height: '400px'
+
+              }
+          }
+        >
+          <img src='../../assets/map_newcastle.png' />
+        </div>
+        <h2
+            style={
+              {
+                textShadow: '2px 2px 1px rgba(0, 0, 0, .8)',
                 color: 'white',
                 marginBottom: '20px',
                 marginTop: '20px',
+                marginRight: '20px',
                 fontFamily: '"Courier New", Courier, monospace',
                 fontWeight: '400',
                 fontSize: '64'
@@ -74,7 +123,29 @@ class ParakeatApp extends Component {
           >
             Dine off the beaten track
           </h2>
+
+
+
         </div>
+        <div
+          className='content-wrapper full-width'
+          style={
+            {
+              background: 'linear-gradient(90deg, rgba(137, 218, 89, .5), rgba(255,255,255,0))',
+              width: '100vw',
+              height: '400px',
+              marginTop: '20px',
+
+
+
+            }
+          }
+        >
+
+
+
+        </div>
+
 
       </div>
     )
--- src/components/ParakeatLogo/index.js
+++ src/components/ParakeatLogo/index.js
@@ -9,7 +9,7 @@ class ParakeatLogo extends Component {
 
   render() {
     const {x, y} = this.props.mousePos;
-    console.log(x,y)
+
     return (
 
       <svg
@@ -21,13 +21,16 @@ class ParakeatLogo extends Component {
       >
 
         <g>
-          <g>
-            <path d="M-122.9,419.5c0-24.7-19-45.1-43.2-47.4l-4.4-0.2l-4.4,0.2c-24.2,2.2-43.2,22.6-43.2,47.4
+          <g transform='rotate(0 -170 420)'>
+            <path
+              transform='rotate(0 -170 420)'
+
+              d="M-122.9,419.5c0-24.7-19-45.1-43.2-47.4l-4.4-0.2l-4.4,0.2c-24.2,2.2-43.2,22.6-43.2,47.4
               c0,26.2,21.3,47.6,47.6,47.6h4.4v-18.4c12.3-2,21.8-12.2,22.8-24.8h20.4L-122.9,419.5L-122.9,419.5z M-174.9,458
               c-19.3-2.2-34.3-18.6-34.3-38.5s15-36.3,34.3-38.5v36.4v6.5v25.2V458z M-166.1,439.7v-13.5h13.5
               C-154.2,432.9-159.4,438.1-166.1,439.7z M-166.1,415.1V381c17.8,2,32.1,16.2,34.1,34.1C-132,415.1-166.1,415.1-166.1,415.1z"/>
             <circle fill='#13aca7' cx="-188.8" cy="404.9" r="6.9"/>
-            <circle ref={this.props.elRef}className='parakeat-pupil' fill='white' cx="-188.8" cy="404.9" r="2"/>
+            <circle ref={this.props.elRef} className='parakeat-pupil' fill='white' cx="-188.8" cy="404.9" r="2"/>
 
          {/*   {this.renderTarget()}*/}
           </g>
